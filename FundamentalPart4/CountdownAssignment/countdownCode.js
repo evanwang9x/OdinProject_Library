4b78ed6fdaeb8a33af6a08e29d424a4eb18cbae8
Evan Wang <evanwang9x@gmail.com>
2023-07-26 10:00:44 -0400

    Finished Countdown Project, Working on Guest List

---
+++ FundamentalPart4/CountdownAssignment/countdownCode.js
@@ -0,0 +1,26 @@
+const btn = document.querySelector("button");
+btn.addEventListener("click", () => {
+    const para = document.createElement("p");
+    para.textContent = "Output: ";
+    
+    for (let i = 10; i >= 0; i--) {
+        if(i === 10) {
+            para.textContent += `Countdown ${i} `
+            continue;
+        }
+        else if (i ===0) {
+            para.textContent += `Blastoff!`
+            continue;
+        }
+        else {
+        para.textContent += `${i} `;
+        }
+    }
+    const output = document.getElementById("output")
+    output.appendChild(para);
+    console.log(para)
+
+
+
+
+});
