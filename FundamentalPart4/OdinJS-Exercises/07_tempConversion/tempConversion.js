08d08cee93c85d328992b0c84b7f12d7c52b8a63
Evan Wang <evanwang9x@gmail.com>
2023-07-27 14:11:32 -0400

    Did tempConversion

--- FundamentalPart4/OdinJS-Exercises/07_tempConversion/tempConversion.js
+++ FundamentalPart4/OdinJS-Exercises/07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(Fahrenheit) {
+  let Celsius = Math.round(((5/9)*(Fahrenheit-32)) * 10) /10
+  if (Celsius < 0) {
+    return -Math.abs(Celsius)
+  }
+  else {
+  return Celsius
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(Celsius) {
+  let Fahrenheit = Math.round(((Celsius*9/5)+32) * 10) / 10
+  if (Fahrenheit < 0) {
+    return -Math.abs(Fahrenheit)
+  }
+  else {
+  return Fahrenheit
+  }
 };
 
 // Do not edit below this line
