40162a9fe01664a3f14a22bcf57807f3e0013e91
Evan Wang <evanwang9x@gmail.com>
2023-07-27 13:53:35 -0400

    Finished Sumall

--- FundamentalPart4/OdinJS-Exercises/05_sumAll/sumAll.js
+++ FundamentalPart4/OdinJS-Exercises/05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(param1, param2) {
+    let sum =0;
+    if(typeof(param1) != "number" || typeof(param2) != "number") {
+        return "ERROR"
+    }
+    else if(param1<0 || param2<0) {
+        return "ERROR"
+    }
+    else if(param1 > param2) {
+        for(let i = param2; i <= param1; i++) {
+            sum += i
+        }
+        return sum
+    }
+    else if(param2 > param1) {
+        for(let i = param1; i <= param2; i++) {
+            sum+=i
+        }
+        return sum
+    }
 };
 
 // Do not edit below this line
