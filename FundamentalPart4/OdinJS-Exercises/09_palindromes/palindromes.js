58f5e0dbd56ca5dcfcf67e3c56e91b882a41da51
Evan Wang <evanwang9x@gmail.com>
2023-08-02 16:03:22 -0400

    Finished Palindromes

--- FundamentalPart4/OdinJS-Exercises/09_palindromes/palindromes.js
+++ FundamentalPart4/OdinJS-Exercises/09_palindromes/palindromes.js
@@ -1,6 +1,29 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let initialWord = ""
+    for(let i = 0; i < word.length; i++) {
+        let tempChar = word[i].charCodeAt(0);
+        if( (tempChar >= 65 && tempChar <= 90) || (tempChar >= 97 && tempChar <= 122) || (tempChar >= 48 && tempChar <= 57)) {
+            initialWord += word[i]
+        }
+    }
+    middleWord = initialWord.toLowerCase()
+    let finalWord = ""
+    for(let i = middleWord.length-1; i >= 0; i--) {
+        if(i === 0) {
+            finalWord+=middleWord[i].toLowerCase()
+        }
 
+        else{
+        finalWord+=middleWord[i]
+        }
+    }
+    console.log(finalWord)
+    if(middleWord == finalWord) {
+        return true
+    }
+    else {
+        return false
+    }
 };
-
 // Do not edit below this line
 module.exports = palindromes;
--- tester.js
+++ tester.js
@@ -1,8 +1,28 @@
-const multiply = function(...args) {
-    let sum = 1;
-    for(let i=0; i < args.length; i++) {
-      sum *= args[i]
+ function dog (word) {
+    let initialWord = ""
+    for(let i = 0; i < word.length; i++) {
+        let tempChar = word[i].charCodeAt(0);
+        if( (tempChar >= 65 && tempChar <= 90) || (tempChar >= 97 && tempChar <= 122) || (tempChar >= 48 && tempChar <= 57)) {
+            initialWord += word[i]
+        }
     }
-    return sum
-  };
-  console.log(multiply(2,4,6))
+    middleWord = initialWord.toLowerCase()
+    let finalWord = ""
+    for(let i = middleWord.length-1; i >= 0; i--) {
+        if(i === 0) {
+            finalWord+=middleWord[i].toLowerCase()
+        }
+
+        else{
+        finalWord+=middleWord[i]
+        }
+    }
+    console.log(finalWord)
+    if(middleWord == finalWord) {
+        return true
+    }
+    else {
+        return false
+    }
+};
+console.log(dog("r3ace3car"))
