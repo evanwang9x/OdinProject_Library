0741f2489f4126ebea5277ec821e5051b5a5982f
Evan Wang <evanwang9x@gmail.com>
2023-08-02 13:21:55 -0400

    Finished calculator.spec.js

--- FundamentalPart4/OdinJS-Exercises/08_calculator/calculator.js
+++ FundamentalPart4/OdinJS-Exercises/08_calculator/calculator.js
@@ -1,26 +1,44 @@
-const add = function() {
-	
+const add = function(num, num2) {
+	return num+num2
 };
 
-const subtract = function() {
-	
+const subtract = function(num, num2) {
+	return num - num2
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+	for(let i = 0; i< array.length; i++) {
+    sum += array[i];
+  }
+  return sum
+}; 
+
+const multiply = function(...args) {
+  let sum = 1;
+  for(let i=0; i < args.length; i++) {
+    sum *= args[i];
+  }
+  return sum;
+};
+const power = function(num, num2) {
+  let sum = 1
+  for(let i =0; i<num2; i++) {
+    sum *= num
+  }
+  return sum;
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+const factorial = function(num) {
+  if (num ===0 ) {
+      return 1
+  }
+  let sum = num
+    for (let i =num-1; i > 0; i--) {
+      sum*= i
+    }
+    return sum;
+  };
 
 // Do not edit below this line
 module.exports = {
