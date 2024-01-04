9a4931ee6ae1de5098e66e88b1b612751b3a84cb
Evan Wang <evanwang9x@gmail.com>
2024-01-04 12:49:26 -0500

    Added basic stuff to the ToDoList

--- OdinFullStackJS/TodoList/projectLibrary.js
+++ OdinFullStackJS/TodoList/projectLibrary.js
@@ -1,7 +1,15 @@
 const headButton = document.getElementById("headerButton")
 const buttons = document.querySelectorAll('.leftHandButtons', ".selectedButton");
+const addTask = document.getElementById("addTask")
+const createTaskPanel = document.getElementById("createTaskPanel")
+const panelAddTask = document.getElementById("addToDo")
+const rightHandContainer = document.getElementById("rightHandContainer")
 
 
+window.onload = function(){
+    button1.classList.remove("leftHandButtons")
+    button1.classList.add("selectedButton")
+}
 
 headButton.addEventListener('click', function() {
     window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"
@@ -26,3 +34,49 @@ buttons.forEach(button => {
         this.classList.add("selectedButton")
     })
 })
+addTask.addEventListener('click', function() {
+    addTask.style.display = 'none';
+    createTaskPanel.style.display = "block"
+})  
+panelAddTask.addEventListener('click', function(){
+    createTaskPanel.style.display = "none"
+    addTask.style.display = "block"
+    var toDoTitle = document.getElementById("toDoTitle").value;
+    var toDoDetails = document.getElementById("toDoDetails").value;
+    var userDate = document.getElementById("myDateInput").value
+
+    var checkbox = document.createElement("input");
+    checkbox.type = 'checkbox';
+    checkbox.className = "completionCheckMark"
+
+    var header1 = document.createElement('h1')
+    header1.className = "titleName"
+    header1.textContent = toDoTitle
+
+    var detailButton = document.createElement('button')
+    detailButton.className = "details"
+    detailButton.textContent = "Details"
+
+    var secondHeader1 = document.createElement('h1')
+    secondHeader1.className = "dateButton"
+    secondHeader1.textContent = userDate
+
+    var editButton = document.createElement("button")
+    editButton.className = "editButton"
+    editButton.textContent = "Edit"
+
+    var deleteButton = document.createElement("button")
+    deleteButton.className = "deleteButton"
+    deleteButton.textContent = "Delete"
+
+    var newDiv = document.createElement('div');
+    newDiv.className = "newDivStyle"
+    newDiv.appendChild(checkbox)
+    newDiv.appendChild(header1)
+    newDiv.appendChild(detailButton)
+    newDiv.appendChild(secondHeader1)
+    newDiv.appendChild(editButton)
+    newDiv.appendChild(deleteButton)
+    rightHandContainer.appendChild(newDiv)
+})
+
